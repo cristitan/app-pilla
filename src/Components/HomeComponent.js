232abae21ea798e997fd9295e6806d0c93db4c11
Cristian <Cristititan@gmail.com>
2020-03-26 20:35:14 -0400

    second commit se prepara componente home

---
+++ src/Components/HomeComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomeComponent = () => (
+      
+      <div class="alert alert-dark" role="alert">
+        Apuntes diarios <a href="#" class="alert-link">Write Please!</a>
+      </div>
+);
+
+export default HomeComponent;
